77d0a00b646c8b5bc29ea87ddadef6fa1134fde0
Deepthi Nidamanuri <deepthi.nidamanuri@heterohcl.com>
2026-01-28 12:19:13 +0530

    Updated Intial commit

--- app.py
+++ app.py
@@ -68,8 +68,8 @@ if not st.session_state.setup_complete:
     with col2:
         st.session_state["position"] = st.selectbox(
             "Choose a position",
-            ("Data Scientist", "Data Engineer", "ML Engineer", "BI Analyst", "Financial Analyst"),
-            index=("Data Scientist", "Data Engineer", "ML Engineer", "BI Analyst", "Financial Analyst").index(st.session_state["position"])
+            ("Software Engineer","Frontend Developer","Backend Developer","Data Scientist", "Data Engineer", "ML Engineer", "BI Analyst", "Financial Analyst",),
+            index=("Software Engineer","Frontend Developer","Backend Developer","Data Scientist", "Data Engineer", "ML Engineer", "BI Analyst", "Financial Analyst",).index(st.session_state["position"])
         )
 
     st.session_state["company"] = st.selectbox(
